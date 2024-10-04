9fe8a6f1a31987c498a203411b211eb9d2d5842d
Ayesha81Siddika <2215151081@uits.edu.bd>
2024-10-05 03:27:27 +0600

    Adding Videos Thumbnails

--- script/video.js
+++ script/video.js
@@ -6,6 +6,35 @@ const loadCatagories = () => {
     .then((data) => displayCatagories(data.categories))
     .catch((err) => console.log(err))
    };
+ const loadVideos = () => {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((err) => console.log(err))
+   };
+
+   const displayVideos = (videos) => {
+      const videoContainer = document.getElementById("videos")
+      videos.forEach( video => {
+          console.log(video)
+          const card = document.createElement("div");
+          card.classList=="card card-compact"
+          card.innerHTML=`<figure>
+    <img
+    src=${video.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+          `
+     videoContainer.append(card)
+      })
+   }
 
 const displayCatagories = (catagories) => {
    const categoryContainer = document.getElementById("catagories");
@@ -25,3 +54,4 @@ const displayCatagories = (catagories) => {
 };
 
 loadCatagories();
+loadVideos();
